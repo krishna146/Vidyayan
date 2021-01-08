20246b7b6e255ad22199f09cc48c2146d8ff60b7
aashutosh-rana <65567507+aashutosh-rana@users.noreply.github.com>
2021-01-08 19:54:37 +0530

    ashutosh-rana-4

--- app/src/main/java/com/bcebhagalpur/welcomeslider/activity/RegistrationActivity.kt
+++ app/src/main/java/com/bcebhagalpur/welcomeslider/activity/RegistrationActivity.kt
@@ -1,6 +1,8 @@
 package com.bcebhagalpur.welcomeslider.activity
 
+import android.Manifest
 import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.*
@@ -8,29 +10,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.bcebhagalpur.welcomeslider.R
 import kotlinx.android.synthetic.main.activity_registration.*
 import java.util.*
-import java.util.jar.Manifest
 
 class RegistrationActivity : AppCompatActivity() {
-    lateinit var lm:LocationManager
-    lateinit var loc:Location
+     private var lm:LocationManager?=null
+     private var loc:Location?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.requestFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
         setContentView(R.layout.activity_registration)
 
-        if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
-            && ActivityCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
+
+        if(ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED)
         {
-          ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_COARSE_LOCATION,android.Manifest.permission.ACCESS_FINE_LOCATION),111)
+          ActivityCompat.requestPermissions(this, arrayOf(ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION),111)
+            return
         }
-        lm=getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        loc=lm.getLastKnownLocation(LocationManager.GPS_PROVIDER)
-        var ll=object:LocationListener{
+       else{
+            lm=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            loc=lm?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+        }
+
+
+        val ll=object:LocationListener{
             override fun onLocationChanged(location: Location?) {
                 reverseGeocode(location)
             }
@@ -38,26 +46,54 @@ class RegistrationActivity : AppCompatActivity() {
 
 
             override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onProviderEnabled(provider: String?) {
-                TODO("Not yet implemented")
+
             }
 
             override fun onProviderDisabled(provider: String?) {
-                TODO("Not yet implemented")
             }
 
 
         }
-        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,100.2f,ll)
+        try {
+            lm!!.requestLocationUpdates(LocationManager.GPS_PROVIDER,1000,100.2f,ll)
+        }catch(e:NullPointerException){
+            e.printStackTrace()
+        }
 
     }
+    @SuppressLint("SetTextI18n")
     private fun reverseGeocode(loc:Location?) {
-        var gc=Geocoder(this@RegistrationActivity, Locale.getDefault())
-        var addresses:List<Address> =gc.getFromLocation(loc!!.latitude,loc.longitude,2)
-        var address:Address=addresses.get(0)
+        val gc=Geocoder(this@RegistrationActivity, Locale.getDefault())
+        val addresses:List<Address> =gc.getFromLocation(loc!!.latitude,loc.longitude,2)
+        val address:Address= addresses[0]
         et_user_address.setText("${address.getAddressLine(0)},${address.locality}")
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
+                                            grantResults: IntArray) {
+        when (requestCode) {
+            111 -> {
+                if (grantResults.isNotEmpty() && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED) {
+                    if(ActivityCompat.checkSelfPermission(this, ACCESS_COARSE_LOCATION)==PackageManager.PERMISSION_GRANTED
+                        && ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+                    {
+                        lm=getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                        loc=lm?.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                        Toast.makeText(this, "permission allowed", Toast.LENGTH_SHORT).show()
+                    }else{
+                        Toast.makeText(this, "some error occurred", Toast.LENGTH_SHORT).show()
+                    }
+                } else {
+                    Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+        }
+    }
+
 }
