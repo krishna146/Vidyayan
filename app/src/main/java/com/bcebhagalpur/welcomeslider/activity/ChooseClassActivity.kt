ed88b7ad07bf7eb296f5dc9df1e6e2f348d4784c
krishna kumar <krishnakumarjms12@gmail.com>
2021-01-10 20:37:13 +0530

    REGISTRATION PAGE CREATED

--- app/src/main/java/com/bcebhagalpur/welcomeslider/activity/ChooseClassActivity.kt
+++ app/src/main/java/com/bcebhagalpur/welcomeslider/activity/ChooseClassActivity.kt
@@ -1,40 +1,110 @@
 package com.bcebhagalpur.welcomeslider.activity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 import android.view.WindowManager
 import com.bcebhagalpur.welcomeslider.R
+import com.bcebhagalpur.welcomeslider.fragment.Claas6thTo10thFragment
+import com.bcebhagalpur.welcomeslider.fragment.Class11thFragment
+import com.bcebhagalpur.welcomeslider.fragment.Class12thFragment
+import com.bcebhagalpur.welcomeslider.fragment.Class13thFragment
 import kotlinx.android.synthetic.main.activity_choose_class.*
 
 class ChooseClassActivity : AppCompatActivity() {
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         window.requestFeature(Window.FEATURE_NO_TITLE)
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+        )
         setContentView(R.layout.activity_choose_class)
-        six.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt13th.setOnClickListener() {
+
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Class13thFragment())
+            transaction.commit()
+            txt13th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+
         }
-        seven.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt12th.setOnClickListener() {
+            txt12th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Class12thFragment())
+            transaction.commit()
         }
-        eight.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt11th.setOnClickListener() {
+            txt11th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Class11thFragment())
+            transaction.commit()
         }
-        nine.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt10th.setOnClickListener(){
+            txt10th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+            supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
         }
-        ten.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+           txt9th.setOnClickListener(){
+               txt9th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+            supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
         }
-        eleven.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt8th.setOnClickListener(){
+            txt8th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
         }
-        twelve.setOnClickListener {
-            startActivity(Intent(this@ChooseClassActivity,RegistrationActivity::class.java))
+        txt7th.setOnClickListener(){
+            txt7th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
         }
+        txt6th.setOnClickListener(){
+            txt6th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+        txt5th.setOnClickListener(){
+            txt5th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+        txt4th.setOnClickListener(){
+            txt4th.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+        txt3rd.setOnClickListener(){
+            txt3rd.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+        txt2nd.setOnClickListener(){
+            txt2nd.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+        txt1st.setOnClickListener(){
+            txt1st.setBackgroundResource(R.drawable.rounded_corners_imagebutton2)
+            val transaction =
+                supportFragmentManager.beginTransaction().replace(R.id.frame, Claas6thTo10thFragment())
+            transaction.commit()
+        }
+
+
     }
 }
