c7285455e86d5adaf9c0838f89404bc0b7bcdd83
ashutoshumar <viraj28300@gmail.com>
2021-01-25 15:55:52 +0530

    ashutoshswipe

--- app/src/main/java/com/bcebhagalpur/welcomeslider/bodyfragment/TeacherFragment.kt
+++ app/src/main/java/com/bcebhagalpur/welcomeslider/bodyfragment/TeacherFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -21,16 +22,18 @@ class TeacherFragment :Fragment() {
     lateinit var recyclerView: RecyclerView
     private lateinit var recyclerAdapter: TEacherAdapter
     private val teacherInfoInfoList = arrayListOf<Teacherlist>()
+    lateinit var searchFragment: SearchFragment
+    //lateinit var blankFragment: BlankFragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_teacher, container, false)
-        recyclerView=view.findViewById(R.id.teachers_list)
+       recyclerView=view.findViewById(R.id.teachers_list)
     //  val teacherobject=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"Computer Wizard","BCE","Taught and mentored more than 1000 students","Taught and mentored more than 1000 students)
 
-        val teacherobject1=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"computer Wizard","BCE","Taught and mentored more than 1000 students","30+ years teaching experience","Secured rank 1 in upsc","90+ iit selection")
+       val teacherobject1=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"computer Wizard","BCE","Taught and mentored more than 1000 students","30+ years teaching experience","Secured rank 1 in upsc","90+ iit selection")
         val teacherobject2=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"computer Wizard","BCE","Taught and mentored more than 1000 students","30+ years teaching experience","Secured rank 1 in upsc","90+ iit selection")
         val teacherobject3=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"computer Wizard","BCE","Taught and mentored more than 1000 students","30+ years teaching experience","Secured rank 1 in upsc","90+ iit selection")
         val teacherobject4=Teacherlist("Suresh Vidyarthi",R.drawable.tp,"computer Wizard","BCE","Taught and mentored more than 1000 students","30+ years teaching experience","Secured rank 1 in upsc","90+ iit selection")
@@ -44,24 +47,7 @@ class TeacherFragment :Fragment() {
         recyclerView.adapter = recyclerAdapter
         recyclerView.layoutManager=
             LinearLayoutManager(activity as Context, LinearLayoutManager.VERTICAL,false)
-        val item=object :Swipe(activity as Context,0,ItemTouchHelper.LEFT){
-            override fun onMove(
-                recyclerView: RecyclerView,
-                viewHolder: RecyclerView.ViewHolder,
-                target: RecyclerView.ViewHolder
-            ): Boolean {
-                var fromPosition=viewHolder.adapterPosition
-                var toPosition=viewHolder.adapterPosition
-                Collections.swap(teacherInfoInfoList,fromPosition,toPosition)
-               // recyclerView.adapter!!.notifyItemMoved(fromPosition,toPosition)
-                return false
-            }
-            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-                Toast.makeText(activity as Context,"Senorita",Toast.LENGTH_SHORT).show()
-            }
-        }
-        val itemTouchHelper=ItemTouchHelper(item)
-        itemTouchHelper.attachToRecyclerView(recyclerView)
+
         return view
     }
 
